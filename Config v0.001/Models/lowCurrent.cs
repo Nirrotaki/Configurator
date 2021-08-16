09d0b3306fbf91f4316bff5d14e10ad0a7c6f9f9
Vladimir <nik_group93@mail.ru>
2021-08-16 20:22:11 +0600

    попытка внести правки с ООП

--- Config v0.001/Models/lowCurrent.cs
+++ Config v0.001/Models/lowCurrent.cs
@@ -3,34 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Config_v0;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Config_v0._001;
 
 namespace Config_v0._001.Models
 {
-    class lowCurrent
+    
+
+    class lowSHNL
     {
 
-        class lowSHNL
-        {
+        MainWindow mainWindow = new MainWindow();
 
+        
+
+        public void radioRUNN_Method(object sender, RoutedEventArgs e)
+        {
+            ComboBox msgBoxSHNL = (sender)ComboBox;
+
+            if (mainWindow.radioRUNN.IsChecked == true)
+            {
+                MessageBox.Show("check");
+            }
 
 
         }
 
-        class lowSHO
-        {
+    }
 
+    class lowSHO
+    {
 
-
-        }
-
-        class RUNN
-        {
-
-
-
-        }
 
 
     }
+
+    class RUNN
+    {
+
+
+
+    }
+
+
+
+
+
+
 }
